e75931696e6063b9d61bc6cfb39eaf6135db7e5f
example@gmail.com <s.griszko@gmail.com>
2021-06-05 20:38:18 +0200

    deploy to netlify

---
+++ build/precache-manifest.b05e4cb6bad83cf63a5222039bc69f25.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c49f9d4e925d4039303953092c731a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5615b4ab4d7c97472e5",
+    "url": "/static/js/2.89548e58.chunk.js"
+  },
+  {
+    "revision": "909ed1e46e0b02101975",
+    "url": "/static/js/main.ab892ff5.chunk.js"
+  },
+  {
+    "revision": "56964df38849b8a1c0fa",
+    "url": "/static/js/runtime-main.cff5f7d7.js"
+  }
+]);
